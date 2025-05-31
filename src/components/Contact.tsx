7877f98a034a34ee288649ec980e146209b6672a
Utsugi0101 <soraki0101@gmail.com>
2025-05-31 21:03:37 +0900

    twitterの変更

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
       <h2 className="text-2xl font-semibold mb-2">Contacts</h2>
       <ul className="contact-list">
         <li>メール：<a href="soraki0101@gmail.com">soraki0101@gmail.com</a></li>
-        <li>X（旧Twitter）：<a href="https://x.com/Utsugi0101" target="_blank" rel="noopener noreferrer">@Utsugi0101</a></li>
+        <li>X（旧Twitter）：<a href="https://x.com/Utsugi0101_dev" target="_blank" rel="noopener noreferrer">@Utsugi0101_dev</a></li>
         <li>GitHub：<a href="https://github.com/Utsugi0101" target="_blank" rel="noopener noreferrer">Utsugi0101</a></li>
         <li>note：<a href="https://note.com/utsugi_studio" target="_blank" rel="noopener noreferrer">うつぎ</a></li>
       </ul>
