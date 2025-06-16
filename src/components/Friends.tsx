d2634b73a5901262c8350611f8a744b4ffc3debe
Utsugi0101 <soraki0101@gmail.com>
2025-06-17 03:58:56 +0900

    かすしゅく.みんなの雛形用意

---
+++ src/components/Friends.tsx
@@ -0,0 +1,11 @@
+export default function Friends() {
+    return (<section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2">Pasts</h2>
+        <ul className="space-y-2">
+        <li>
+            <a href="   https://utsugi0101.github.io/utsugi0101/" target="_blank" rel="noopener noreferrer">Utsugi0101</a> - うつぎのポートフォリオサイト
+        </li>
+        </ul>
+      </section>
+    )
+};
